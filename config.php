84d93836873612e4e98d0a6ee9378e793e697036
Raiu1210 <baio1484@gmail.com>
2017-12-10 23:30:18 +0900

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define("consumer_key", "YHuXWt1VdiQxYFwclyYoE7zR7");
-define('consumer_secret', '6daNsvpxEhAOJgrUYVvIlmgYoPVPr1tjlGaatEXihdfTr0gret');
-define('access_token', '939296377632210944-Ho14kav29TKGKQLYKm7yCQnmCMENXFB');
-define('access_token_secret', 'KS0YMmy5NV6sykQXlrqRuAfiU7mppHYjUHrhaj0pUcW7d');
+define("consumer_key", "");
+define('consumer_secret', '');
+define('access_token', '');
+define('access_token_secret', '');
 ?>
